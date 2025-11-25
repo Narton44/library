0f21cb92f4a143a0692211bba1da8c5a7df6f346
Narton44 <sosokom5522@gmail.com>
2025-11-25 15:59:16 +0300

    add book class

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class bookinstance(models.Model):
+    name = models.CharField(
+        verbose_name='название',
+        max_length=30
+    )
+
+    author = models.CharField(
+        verbose_name='автор',
+        max_length=20
+    )
+
+    quantity = models.PositiveSmallIntegerField(
+        verbose_name='количество',
+
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+
+    
